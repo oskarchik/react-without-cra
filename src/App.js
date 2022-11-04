978fb7387eea4b035b5caba58baddcb329857c1f
Oscar Sanz <osanzce@gmail.com>
2022-11-04 08:23:13 +0100

    feat: add starting files
    Prints hello world message on screen to prove that app works

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const App = () => {
+  return (
+    <div className='app'>
+      <h1 className='greet'>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './styles/index.scss';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
